7efecf642db3e4f08b7ee8269be2b6305e5309a6
Ashwin <arora.ashwin95@gmail.com>
2020-01-31 19:46:51 +0100

    Changed Activity type Acc to Dwaraka Commit

--- app/models/activityType.js
+++ app/models/activityType.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ActivityType = Object.freeze({
-    Walking: 'walking',
-    Cycling: 'cycling',
-    Running: 'running',
-    Swimming: 'swimming',
-    Sleeping: 'sleeping'
+    Walking: 'WALKING',
+    Cycling: 'BICYCLE',
+    Running: 'RUNNING',
+    Vehicle: 'VEHICLE',
+    Sleeping: 'SLEEP'
 })
 
 exports = ActivityType;
