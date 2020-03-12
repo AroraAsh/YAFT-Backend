ca17e3c4d3cc73454358cb4ece5dabef50557fc3
Neoamos <aonews@gmail.com>
2020-03-12 10:57:16 -0600

    Changed format of /achievements/list and added user values if user is logged in

--- app_api/controllers/achievementController.js
+++ app_api/controllers/achievementController.js
@@ -1,6 +1,20 @@
 'use strict';
 
 const AchievementTypes = require('../../app/models/achievementTypes');
+var Models = require('../../app/models')
+
 exports.list = async function(req, res){
-  return res.json(AchievementTypes.getAchievementTypes())
+  var achievementTypes = AchievementTypes.getAchievementTypes()
+  if(req.user){
+    var user = await Models.User.findOne({ email: req.user.email });
+    user.achievements.forEach(function(achievement){
+      if(achievementTypes[achievement.type]){
+        achievementTypes[achievement.type].value = achievement.value
+      }
+    })
+  }
+  return res.json({
+    status: "success",
+    result: achievementTypes
+  })
 }
